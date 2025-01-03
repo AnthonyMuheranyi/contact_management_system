dca5d6017ecf51a180c30d53dff78710c5426681
AnthonyMuheranyi <anthonymuheranyi@gmail.com>
2025-01-03 15:42:47 +0000

    added the if conditions

--- contact_mangement_system.cpp
+++ contact_mangement_system.cpp
@@ -25,4 +25,19 @@ vector<Contact> loadContacts() {
     vector<Contact> contacts;
     ifstream file("contacts.txt");
     if (file.is_open()) {
-       
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
+            string name, phone, email;
+            getline(ss, name, ',');
+            getline(ss, phone, ',');
+            getline(ss, email, ',');
+            contacts.push_back({name, phone, email});
+        }
+        file.close();
+    } else {
+        cout << "Error: Unable to open file for reading.\n";
+    }
+    return contacts;
+}
+
